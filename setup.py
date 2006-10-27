b7188774ff33fc9e79f5830608c2bf9a44da446f
scoder <none@none>
2006-10-27 13:12:14 +0200

    [svn r2013] unused PYTHONPATH modification removed
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -100,8 +100,6 @@ print "Building lxml version", svn_version
 
 # setup etree extension building
 
-sys.path.insert(0, os.path.join(src_dir, 'pyrex')) # in case we use a local Pyrex
-
 try:
     from Pyrex.Distutils import build_ext as build_pyx
     source_extension = ".pyx"
