0deeb4514b72cd8428bb386a05c7b91024bd3afa
scoder <none@none>
2007-02-21 16:43:46 +0100

    [svn r2126] first take on a major split-up of api.txt
    --HG--
    branch : trunk
    rename : doc/api.txt => doc/parsing.txt
    rename : doc/api.txt => doc/validation.txt
    rename : doc/api.txt => doc/xpathxslt.txt

--- doc/mkhtml.py
+++ doc/mkhtml.py
@@ -14,7 +14,8 @@ def publish(dirname, lxml_path, release):
     for name in ['main.txt', 'intro.txt', 'api.txt', 'compatibility.txt',
                  'extensions.txt', 'element_classes.txt', 'sax.txt',
                  'build.txt', 'FAQ.txt', 'performance.txt', 'resolvers.txt',
-                 'capi.txt', 'objectify.txt']:
+                 'capi.txt', 'objectify.txt', 'validation.txt',
+                 'xpathxslt.txt', 'parsing.txt']:
         path = os.path.join(doc_dir, name)
         outname = os.path.splitext(name)[0] + '.html'
         outpath = os.path.join(dirname, outname)
