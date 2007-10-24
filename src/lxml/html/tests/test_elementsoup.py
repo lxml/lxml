5293eac21c08dc2615365b6a98bc267ff117601c
scoder <none@none>
2007-10-24 14:50:30 +0200

    [svn r2991] skip ElementSoup test if BeautifulSoup is not installed
    --HG--
    branch : trunk

--- src/lxml/html/tests/test_elementsoup.py
+++ src/lxml/html/tests/test_elementsoup.py
@@ -1,9 +1,17 @@
 import unittest
 from lxml.tests.common_imports import doctest
 
+try:
+    import BeautifulSoup
+    BS_INSTALLED = True
+except:
+    BS_INSTALLED = False
+
+
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTests([doctest.DocFileSuite('../../../../doc/elementsoup.txt')])
+    if BS_INSTALLED:
+        suite.addTests([doctest.DocFileSuite('../../../../doc/elementsoup.txt')])
     return suite
 
 if __name__ == '__main__':
