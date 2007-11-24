ea74c475a7eac4803e53e71f8390158197080588
scoder <none@none>
2007-11-24 11:00:38 +0100

    [svn r3061] split of 'clean embed' test case which fails in libxml2 2.6.29/30
    --HG--
    branch : trunk

--- src/lxml/html/tests/test_clean.py
+++ src/lxml/html/tests/test_clean.py
@@ -1,7 +1,10 @@
 import unittest
 from lxml.tests.common_imports import doctest
+from lxml.etree import LIBXML_VERSION
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([doctest.DocFileSuite('test_clean.txt')])
+    if LIBXML_VERSION <= (2,6,28):
+        suite.addTests([doctest.DocFileSuite('test_clean_embed.txt')])
     return suite
