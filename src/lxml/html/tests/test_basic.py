8a6571da3380e4e5f0a70e15d1393b617e1a6374
scoder <none@none>
2008-01-14 19:54:30 +0100

    [svn r3174] r3261@delle:  sbehnel | 2008-01-14 07:23:28 +0100  lxmlhtml.txt doesn't work as doctest
    --HG--
    branch : trunk

--- src/lxml/html/tests/test_basic.py
+++ src/lxml/html/tests/test_basic.py
@@ -4,7 +4,6 @@ from lxml.tests.common_imports import doctest
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([doctest.DocFileSuite('test_basic.txt')])
-    suite.addTests([doctest.DocFileSuite('../../../../doc/lxmlhtml.txt')])
     return suite
 
 if __name__ == '__main__':
