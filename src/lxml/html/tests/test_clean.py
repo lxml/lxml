19e9417c8bca6c5e5e6bbf63fc7b63acd83485ee
scoder <none@none>
2008-01-26 17:44:34 +0100

    [svn r3212] r3336@delle:  sbehnel | 2008-01-26 17:44:10 +0100  get rid of test failures for older libxml2 versions
    --HG--
    branch : trunk

--- src/lxml/html/tests/test_clean.py
+++ src/lxml/html/tests/test_clean.py
@@ -6,6 +6,6 @@ def test_suite():
     suite = unittest.TestSuite()
     if sys.version_info >= (2,4):
         suite.addTests([doctest.DocFileSuite('test_clean.txt')])
-        if LIBXML_VERSION <= (2,6,28) or LIBXML_VERSION >= (2,6,31):
+        if LIBXML_VERSION >= (2,6,31):
             suite.addTests([doctest.DocFileSuite('test_clean_embed.txt')])
     return suite
