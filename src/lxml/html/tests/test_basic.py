aa3f625a860feaf17c79d47d7ff6feab5b2fa132
scoder <none@none>
2008-02-22 08:49:46 +0100

    [svn r3334] r3585@delle:  sbehnel | 2008-02-22 08:48:34 +0100  enable doctests in lxml.html
    --HG--
    branch : trunk

--- src/lxml/html/tests/test_basic.py
+++ src/lxml/html/tests/test_basic.py
@@ -1,10 +1,12 @@
 import unittest, sys
 from lxml.tests.common_imports import doctest
+import lxml.html
 
 def test_suite():
     suite = unittest.TestSuite()
     if sys.version_info >= (2,4):
         suite.addTests([doctest.DocFileSuite('test_basic.txt')])
+    suite.addTests([doctest.DocTestSuite(lxml.html)])
     return suite
 
 if __name__ == '__main__':
