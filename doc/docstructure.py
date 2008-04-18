d829ebcce1fe9c762de4730eabcc9260ecb575f6
scoder <none@none>
2008-04-18 21:42:35 +0200

    [svn r3558] r4002@delle:  sbehnel | 2008-04-18 21:38:47 +0200  external module to describe doc structure
    --HG--
    branch : trunk

---
+++ doc/docstructure.py
@@ -0,0 +1,24 @@
+
+SITE_STRUCTURE = [
+    ('lxml', ('main.txt', 'intro.txt', '../INSTALL.txt', 'lxml2.txt',
+              'performance.txt', 'compatibility.txt', 'FAQ.txt')),
+    ('Developing with lxml', ('tutorial.txt', '@API reference',
+                              'api.txt', 'parsing.txt',
+                              'validation.txt', 'xpathxslt.txt',
+                              'objectify.txt', 'lxmlhtml.txt',
+                              'cssselect.txt', 'elementsoup.txt')),
+    ('Extending lxml', ('resolvers.txt', 'extensions.txt',
+                        'element_classes.txt', 'sax.txt', 'capi.txt')),
+    ('Developing lxml', ('build.txt', 'lxml-source-howto.txt',
+                         '@Release Changelog', '../CREDITS.txt')),
+    ]
+
+HREF_MAP = {
+    "API reference" : "api/index.html"
+}
+
+BASENAME_MAP = {
+    'main' : 'index',
+    'INSTALL' : 'installation',
+    'CREDITS' : 'credits',
+}
--- doc/mkhtml.py
+++ doc/mkhtml.py
@@ -1,20 +1,7 @@
+from docstructure import SITE_STRUCTURE, HREF_MAP, BASENAME_MAP
 from lxml.etree import (parse, fromstring, ElementTree,
                         Element, SubElement, XPath)
 import os, shutil, re, sys, copy, time
-
-SITE_STRUCTURE = [
-    ('lxml', ('main.txt', 'intro.txt', '../INSTALL.txt', 'lxml2.txt',
-              'performance.txt', 'compatibility.txt', 'FAQ.txt')),
-    ('Developing with lxml', ('tutorial.txt', '@API reference',
-                              'api.txt', 'parsing.txt',
-                              'validation.txt', 'xpathxslt.txt',
-                              'objectify.txt', 'lxmlhtml.txt',
-                              'cssselect.txt', 'elementsoup.txt')),
-    ('Extending lxml', ('resolvers.txt', 'extensions.txt',
-                        'element_classes.txt', 'sax.txt', 'capi.txt')),
-    ('Developing lxml', ('build.txt', 'lxml-source-howto.txt',
-                         '@Release Changelog')),
-    ]
 
 RST2HTML_OPTIONS = " ".join([
     "--no-toc-backlinks",
@@ -22,15 +9,6 @@ RST2HTML_OPTIONS = " ".join([
     "--language en",
     "--date",
     ])
-
-HREF_MAP = {
-    "API reference" : "api/index.html"
-}
-
-BASENAME_MAP = {
-    'main' : 'index',
-    'INSTALL' : 'installation',
-}
 
 htmlnsmap = {"h" : "http://www.w3.org/1999/xhtml"}
 
