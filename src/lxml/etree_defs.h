81f595fa534a89a877012a2e4cd72f15cad9e856
scoder <none@none>
2008-05-08 17:45:10 +0200

    [svn r3666] r4194@delle:  sbehnel | 2008-05-08 15:18:11 +0200  removed unused #define section: already provided by Cython
    --HG--
    branch : trunk

--- src/lxml/etree_defs.h
+++ src/lxml/etree_defs.h
@@ -5,17 +5,6 @@
 #define va_int(ap)     va_arg(ap, int)
 #define va_charptr(ap) va_arg(ap, char *)
 
-/* Py_ssize_t support was added in Python 2.5 */
-#if PY_VERSION_HEX < 0x02050000
-#  ifndef PY_SSIZE_T_MAX /* patched Pyrex? */
-     typedef int Py_ssize_t;
-#    define PY_SSIZE_T_MAX INT_MAX
-#    define PY_SSIZE_T_MIN INT_MIN
-#    define PyInt_FromSsize_t(z) PyInt_FromLong(z)
-#    define PyInt_AsSsize_t(o)   PyInt_AsLong(o)
-#  endif
-#endif
-
 /* Threading can crash under Python <= 2.4.1 */
 #if PY_VERSION_HEX < 0x02040200
 #  ifndef WITHOUT_THREADING
