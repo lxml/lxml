a57082187bae1297ee4af35cbc2f069e84a0640d
scoder <none@none>
2008-05-20 00:00:23 +0200

    [svn r3680] r4226@delle:  sbehnel | 2008-05-19 00:52:10 +0200  initial Py3 fixes for error constant parsing script
    --HG--
    branch : trunk

--- update-error-constants.py
+++ update-error-constants.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-import sys, os, os.path, re
+import sys, os, os.path, re, codecs
 
 BUILD_SOURCE_FILE = os.path.join("src", "lxml", "xmlerror.pxi")
 BUILD_DEF_FILE    = os.path.join("src", "lxml", "xmlerror.pxd")
 
 if len(sys.argv) < 2 or sys.argv[1].lower() in ('-h', '--help'):
-    print "This script generates the constants in file", BUILD_SOURCE_FILE
-    print "Call as"
-    print sys.argv[0], "/path/to/libxml2-doc-dir"
+    print("This script generates the constants in file %s" % BUILD_SOURCE_FILE)
+    print("Call as")
+    print(sys.argv[0], "/path/to/libxml2-doc-dir")
     sys.exit(len(sys.argv) > 1)
 
 HTML_DIR = os.path.join(sys.argv[1], 'html')
@@ -58,12 +58,12 @@ def split(lines):
 
 def regenerate_file(filename, result):
     # read .pxi source file
-    f = open(filename, 'r')
+    f = codecs.open(filename, 'r', encoding="utf-8")
     pre, post = split(f)
     f.close()
 
     # write .pxi source file
-    f = open(filename, 'w')
+    f = codecs.open(filename, 'w', encoding="utf-8")
     f.write(''.join(pre))
     f.write(COMMENT)
     f.write('\n'.join(result))
@@ -87,7 +87,7 @@ def parse_enums(html_dir, html_filename, enum_dict):
         enum_name = enum_name.group(1)
         if enum_name not in ENUM_MAP:
             continue
-        print "Found enum", enum_name
+        print("Found enum", enum_name)
         entries = []
         for child in enum:
             name = child.text
@@ -132,7 +132,7 @@ for enum_name in ENUM_ORDER:
 
     append_pxd(ctypedef_indent + 'ctypedef enum %s:' % enum_name)
     append_pxi('cdef object %s' % pxi_name)
-    append_pxi('%s = ("""\\' % pxi_name)
+    append_pxi('%s = (u"""\\' % pxi_name)
 
     prefix_len = len(prefix)
     length = 2 # each string ends with '\n\0'
@@ -148,7 +148,7 @@ for enum_name in ENUM_ORDER:
         line = '%s=%d' % (name, val)
         if length + len(line) >= 2040: # max string length in MSVC is 2048
             append_pxi('""",')
-            append_pxi('"""\\')
+            append_pxi('u"""\\')
             length = 2 # each string ends with '\n\0'
         append_pxi(line)
         length += len(line) + 2 # + '\n\0'
@@ -158,10 +158,10 @@ for enum_name in ENUM_ORDER:
     append_pxi('')
 
 # write source files
-print "Updating file", BUILD_SOURCE_FILE
+print("Updating file %s" % BUILD_SOURCE_FILE)
 regenerate_file(BUILD_SOURCE_FILE, pxi_result)
 
-print "Updating file", BUILD_DEF_FILE
+print("Updating file %s" % BUILD_DEF_FILE)
 regenerate_file(BUILD_DEF_FILE,    pxd_result)
 
-print "Done"
+print("Done")
