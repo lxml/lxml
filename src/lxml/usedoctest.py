069f5c3ff741ed0c8a711e3e71371e59111a5095
scoder <none@none>
2008-05-24 10:09:18 +0200

    [svn r3736] r4331@delle:  sbehnel | 2008-05-24 09:50:05 +0200  disable usedoctest in Py3 for now, doesn't work reliably
    --HG--
    branch : trunk

--- src/lxml/usedoctest.py
+++ src/lxml/usedoctest.py
@@ -8,6 +8,8 @@ Usage::
 See `lxml.doctestcompare`
 """
 
+import sys
 from lxml import doctestcompare
 
-doctestcompare.temp_install(del_module=__name__)
+if sys.version_info[0] < 3:
+    doctestcompare.temp_install(del_module=__name__)
