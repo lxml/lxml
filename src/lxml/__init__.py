bdf88fab2baed84eff7ecdf49d0c866dfd13ea30
Stefan Behnel <scoder@users.berlios.de>
2011-09-01 08:54:15 +0200

    rename lxml.get_includes() to lxml.get_include() - sounds wrong, but there is more precedence for that

--- src/lxml/__init__.py
+++ src/lxml/__init__.py
@@ -2,7 +2,7 @@
 
 import os
 
-def get_includes():
+def get_include():
     """
     Returns a list of header include paths (for lxml itself, libxml2
     and libxslt) needed to compile C code against lxml if it was built
