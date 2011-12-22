740445852ffbd6600b9693983abf432388b7dbc3
Matteo Bertozzi <theo.bertozzi@gmail.com>
2011-12-22 16:18:41 +0100

    copy all .h and .pxd deps

--- setup.py
+++ setup.py
@@ -71,7 +71,11 @@ extra_options['package_data'] = {
     'lxml': [
         'etreepublic.pxd',
         'tree.pxd',
-        'etree_defs.h'
+        'cstd.pxd',
+        'etree_defs.h',
+        'lxml-version.h',
+        'lxml.etree_api.h',
+        'lxml.etree.h',
         ],
     'lxml.isoschematron':  [
         'resources/rng/iso-schematron.rng',
