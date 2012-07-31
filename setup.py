f7578c4d4c37e29d5d3bd4a79942bcd617c33f79
Stefan Behnel <stefan_ml@behnel.de>
2012-07-31 16:42:48 +0200

    we don't really support Py3.0 anymore

--- setup.py
+++ setup.py
@@ -213,7 +213,6 @@ an appropriate version of Cython installed.
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.0',
     'Programming Language :: Python :: 3.1',
     'Programming Language :: Python :: 3.2',
     'Programming Language :: C',
