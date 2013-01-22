07736236ca105eed4f55ed9d50db296aaf339bea
Stefan Behnel <stefan_ml@behnel.de>
2013-01-22 08:24:10 +0100

    add test for using different nsmaps in el.find*()
    --HG--
    extra : transplant_source : %7Dd%8Fs%A5%CEp%F3%9Eco%15%ACDr%21%C8%F0%2B%B0

--- src/lxml/tests/test_etree.py
+++ src/lxml/tests/test_etree.py
@@ -2400,6 +2400,18 @@ class ETreeOnlyTestCase(HelperTestCase):
         self.assertEquals(len(root.findall(".//{X}*")), 2)
         self.assertEquals(len(root.findall(".//b")), 3)
 
+    def test_findall_different_nsmaps(self):
+        XML = self.etree.XML
+        root = XML(_bytes('<a xmlns:x="X" xmlns:y="Y"><x:b><c/></x:b><b/><c><x:b/><b/></c><y:b/></a>'))
+        nsmap = {'xx': 'X'}
+        self.assertEqual(len(root.findall(".//xx:b", namespaces=nsmap)), 2)
+        self.assertEqual(len(root.findall(".//xx:*", namespaces=nsmap)), 2)
+        self.assertEqual(len(root.findall(".//b", namespaces=nsmap)), 2)
+        nsmap = {'xx': 'Y'}
+        self.assertEqual(len(root.findall(".//xx:b", namespaces=nsmap)), 1)
+        self.assertEqual(len(root.findall(".//xx:*", namespaces=nsmap)), 1)
+        self.assertEqual(len(root.findall(".//b", namespaces=nsmap)), 2)
+
     def test_findall_syntax_error(self):
         XML = self.etree.XML
         root = XML(_bytes('<a><b><c/></b><b/><c><b/><b/></c><b/></a>'))
