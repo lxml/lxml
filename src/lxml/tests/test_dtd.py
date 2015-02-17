0751b132249e15c4e64058d5a822ac9de702b963
Stefan Behnel <stefan_ml@behnel.de>
2015-02-17 10:16:09 +0100

    fix test in Py3.2 (which has no 'u' string prefix)

--- src/lxml/tests/test_dtd.py
+++ src/lxml/tests/test_dtd.py
@@ -388,7 +388,7 @@ class ETreeDtdTestCase(HelperTestCase):
         def set_public_id(value):
             docinfo.public_id = value
         self.assertRaises(ValueError, set_public_id, _str('ä'))
-        self.assertRaises(ValueError, set_public_id, u'qwerty \xe4\xf6 asdf')
+        self.assertRaises(ValueError, set_public_id, _str('qwerty \xe4\xf6 asdf'))
 
     def test_invalid_decl_2(self):
         docinfo = etree.Element('test').getroottree().docinfo
