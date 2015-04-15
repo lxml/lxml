bbc6475a486d56a62608d37c723cf1fa7a905fc6
Stefan Behnel <stefan_ml@behnel.de>
2015-04-15 19:07:57 +0200

    add initial test for elementpath implementation

---
+++ src/lxml/tests/test_elementpath.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests for the ElementTree API
+
+Only test cases that apply equally well to etree and ElementTree
+belong here. Note that there is a second test module called test_io.py
+for IO related test cases.
+"""
+
+from __future__ import absolute_import
+
+import unittest
+from .common_imports import etree, HelperTestCase
+
+
+class EtreeElementPathTestCase(HelperTestCase):
+    etree = etree
+    from lxml import _elementpath
+
+    def test_cache(self):
+        self._elementpath._cache.clear()
+        el = self.etree.XML(b'<a><b><c/><c/></b></a>')
+        self.assertFalse(self._elementpath._cache)
+
+        self.assertTrue(el.findall('b/c'))
+        self.assertEqual(1, len(self._elementpath._cache))
+        self.assertTrue(el.findall('b/c'))
+        self.assertEqual(1, len(self._elementpath._cache))
+        self.assertFalse(el.findall('xxx'))
+        self.assertEqual(2, len(self._elementpath._cache))
+        self.assertFalse(el.findall('xxx'))
+        self.assertEqual(2, len(self._elementpath._cache))
+        self.assertTrue(el.findall('b/c'))
+        self.assertEqual(2, len(self._elementpath._cache))
+
+
+class ElementTreeElementPathTestCase(EtreeElementPathTestCase):
+    import xml.etree.ElementTree as etree
+    import xml.etree.ElementPath as _elementpath
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([unittest.makeSuite(ElementTreeElementPathTestCase)])
+    suite.addTests([unittest.makeSuite(EtreeElementPathTestCase)])
+    return suite
+
+
+if __name__ == '__main__':
+    print('to test use test.py %s' % __file__)
