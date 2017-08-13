9ccbce92e706a1d28f3f98ce06f46f579910ff2a
Stefan Behnel <stefan_ml@behnel.de>
2017-08-13 09:12:17 +0200

    LP#1710429: Fix uninitialised variable usage.

--- src/lxml/html/_diffcommand.py
+++ src/lxml/html/_diffcommand.py
@@ -73,6 +73,7 @@ body_end_re = re.compile(
     r"</body.*?>", re.I|re.S)
     
 def split_body(html):
+    pre = post = ''
     match = body_start_re.search(html)
     if match:
         pre = html[:match.end()]
