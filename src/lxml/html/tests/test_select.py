df3b82133f3ffa73287b89ac43841639096e9128
Stefan Behnel <stefan_ml@behnel.de>
2017-11-12 18:12:04 +0100

    Clean up test code for better readability.

--- src/lxml/html/tests/test_select.py
+++ src/lxml/html/tests/test_select.py
@@ -6,7 +6,7 @@ import lxml.html
 
 class SelectTest(unittest.TestCase):
     @staticmethod
-    def _evaluate_select(multiple, options):
+    def _evaluate_select(options, multiple=False):
         options = ''.join('<option' + (' selected="selected"' if selected else '') + '>' + option + '</option>'
                           for option, selected in options)
         string = '<title>test</title><form><select%s>%s</select></form>' % \
@@ -14,22 +14,32 @@ class SelectTest(unittest.TestCase):
         return lxml.html.fromstring(string).find('.//select').value
 
     def test_single_select_value_no_options(self):
-        self.assertEqual(self._evaluate_select(False, []), None)
+        self.assertEqual(
+            self._evaluate_select([]),
+            None)
 
     def test_single_select_value_no_selected_option(self):
         # If no option is selected, the HTML5 specification requires the first option to get selected.
-        self.assertEqual(self._evaluate_select(False, [('a', False), ('b', False)]), 'a')
+        self.assertEqual(
+            self._evaluate_select([('a', False), ('b', False)]),
+            'a')
 
     def test_single_select_value_multiple_selected_options(self):
         # If multiple options are selected, the proposed HTML 5.1 specification
         # requires all but the last selected options to get deselected.
-        self.assertEqual(self._evaluate_select(False, [('a', True), ('b', True)]), 'b')
+        self.assertEqual(
+            self._evaluate_select([('a', True), ('b', True)]),
+            'b')
 
     def test_multiple_select_value_no_selected_option(self):
-        self.assertEqual(self._evaluate_select(True, [('a', False), ('b', False)]), set())
+        self.assertEqual(
+            self._evaluate_select([('a', False), ('b', False)], multiple=True),
+            set())
 
     def test_multiple_select_value_multiple_selected_options(self):
-        self.assertEqual(self._evaluate_select(True, [('a', True), ('b', True)]), set(['a', 'b']))
+        self.assertEqual(
+            self._evaluate_select([('a', True), ('b', True)], multiple=True),
+            set(['a', 'b']))
 
 
 def test_suite():
