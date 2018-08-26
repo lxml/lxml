37f87ef29780db7db998e9e17a3281720455e244
Hugo <hugovk@users.noreply.github.com>
2018-08-26 15:08:34 +0300

    'assert False' more readable than 'assert 0'

--- src/lxml/html/diff.py
+++ src/lxml/html/diff.py
@@ -621,7 +621,7 @@ def fixup_chunks(chunks):
                     % (cur_word, result, chunk, chunks))
                 cur_word.post_tags.append(chunk)
         else:
-            assert 0
+            assert False
 
     if not result:
         return [token('', pre_tags=tag_accum)]
