c1732d3bbf5bc46d4f91c3a2f45cc88125083d88
Stefan Behnel <stefan_ml@behnel.de>
2019-04-26 18:07:08 +0200

    Add C14N 2.0 implementation.

--- src/lxml/tests/test_elementtree.py
+++ src/lxml/tests/test_elementtree.py
@@ -9,13 +9,15 @@ for IO related test cases.
 """
 
 import copy
+import io
 import operator
 import os
 import re
 import sys
 import textwrap
 import unittest
-from functools import wraps
+from contextlib import contextmanager
+from functools import wraps, partial
 from itertools import islice
 
 this_dir = os.path.dirname(__file__)
@@ -4637,6 +4639,171 @@ class _XMLPullParserTest(unittest.TestCase):
             self.etree.XMLPullParser(events=('start', 'end', 'bogus'))
 
 
+class _C14NTest(unittest.TestCase):
+    etree = None
+    maxDiff = None
+
+    if not hasattr(unittest.TestCase, 'subTest'):
+        @contextmanager
+        def subTest(self, name):
+            try:
+                yield
+            except Exception as e:
+                print("Subtest {} failed: {}".format(name, e))
+                raise
+
+    #
+    # simple roundtrip tests (from c14n.py)
+
+    def c14n_roundtrip(self, xml, **options):
+        f = io.StringIO()
+        self.etree.canonicalize(f.write, xml, **options)
+        return f.getvalue()
+
+    def test_simple_roundtrip(self):
+        c14n_roundtrip = self.c14n_roundtrip
+        # Basics
+        self.assertEqual(c14n_roundtrip("<doc/>"), '<doc></doc>')
+        self.assertEqual(c14n_roundtrip("<doc xmlns='uri'/>"), # FIXME
+                '<doc xmlns="uri"></doc>')
+        self.assertEqual(c14n_roundtrip("<prefix:doc xmlns:prefix='uri'/>"),
+            '<prefix:doc xmlns:prefix="uri"></prefix:doc>')
+        self.assertEqual(c14n_roundtrip("<doc xmlns:prefix='uri'><prefix:bar/></doc>"),
+            '<doc><prefix:bar xmlns:prefix="uri"></prefix:bar></doc>')
+        self.assertEqual(c14n_roundtrip("<elem xmlns:wsu='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd' xmlns:SOAP-ENV='http://schemas.xmlsoap.org/soap/envelope/' />"),
+            '<elem></elem>')
+
+        # C14N spec
+        self.assertEqual(c14n_roundtrip("<doc>Hello, world!<!-- Comment 1 --></doc>"),
+            '<doc>Hello, world!</doc>')
+        self.assertEqual(c14n_roundtrip("<value>&#x32;</value>"),
+            '<value>2</value>')
+        self.assertEqual(c14n_roundtrip('<compute><![CDATA[value>"0" && value<"10" ?"valid":"error"]]></compute>'),
+            '<compute>value&gt;"0" &amp;&amp; value&lt;"10" ?"valid":"error"</compute>')
+        self.assertEqual(c14n_roundtrip('''<compute expr='value>"0" &amp;&amp; value&lt;"10" ?"valid":"error"'>valid</compute>'''),
+            '<compute expr="value>&quot;0&quot; &amp;&amp; value&lt;&quot;10&quot; ?&quot;valid&quot;:&quot;error&quot;">valid</compute>')
+        self.assertEqual(c14n_roundtrip("<norm attr=' &apos;   &#x20;&#13;&#xa;&#9;   &apos; '/>"),
+            '<norm attr=" \'    &#xD;&#xA;&#x9;   \' "></norm>')
+        self.assertEqual(c14n_roundtrip("<normNames attr='   A   &#x20;&#13;&#xa;&#9;   B   '/>"),
+            '<normNames attr="   A    &#xD;&#xA;&#x9;   B   "></normNames>')
+        self.assertEqual(c14n_roundtrip("<normId id=' &apos;   &#x20;&#13;&#xa;&#9;   &apos; '/>"),
+            '<normId id=" \'    &#xD;&#xA;&#x9;   \' "></normId>')
+
+        # fragments from PJ's tests
+        #self.assertEqual(c14n_roundtrip("<doc xmlns:x='http://example.com/x' xmlns='http://example.com/default'><b y:a1='1' xmlns='http://example.com/default' a3='3' xmlns:y='http://example.com/y' y:a2='2'/></doc>"),
+        #'<doc xmlns:x="http://example.com/x"><b xmlns:y="http://example.com/y" a3="3" y:a1="1" y:a2="2"></b></doc>')
+
+    #
+    # basic method=c14n tests from the c14n 2.0 specification.  uses
+    # test files under xmltestdata/c14n-20.
+
+    # note that this uses generated C14N versions of the standard ET.write
+    # output, not roundtripped C14N (see above).
+
+    def test_xml_c14n2(self):
+        datadir = os.path.join(os.path.dirname(__file__), "c14n-20")
+        full_path = partial(os.path.join, datadir)
+
+        files = [filename[:-4] for filename in sorted(os.listdir(datadir))
+                 if filename.endswith('.xml')]
+        input_files = [
+            filename for filename in files
+            if filename.startswith('in')
+        ]
+        configs = {
+            filename: {
+                # <c14n2:PrefixRewrite>sequential</c14n2:PrefixRewrite>
+                option.tag.split('}')[-1]: ((option.text or '').strip(), option)
+                for option in self.etree.parse(full_path(filename) + ".xml").getroot()
+            }
+            for filename in files
+            if filename.startswith('c14n')
+        }
+
+        tests = {
+            input_file: [
+                (filename, configs[filename.rsplit('_', 1)[-1]])
+                for filename in files
+                if filename.startswith('out_%s_' % input_file)
+                and filename.rsplit('_', 1)[-1] in configs
+            ]
+            for input_file in input_files
+        }
+
+        # Make sure we found all test cases.
+        self.assertEqual(30, len([
+            output_file for output_files in tests.values()
+            for output_file in output_files]))
+
+        def get_option(config, option_name, default=None):
+            return config.get(option_name, (default, ()))[0]
+
+        for input_file, output_files in tests.items():
+            for output_file, config in output_files:
+                keep_comments = get_option(
+                    config, 'IgnoreComments') == 'true'  # no, it's right :)
+                strip_text = get_option(
+                    config, 'TrimTextNodes') == 'true'
+                rewrite_prefixes = get_option(
+                    config, 'PrefixRewrite') == 'sequential'
+                if 'QNameAware' in config:
+                    qattrs = [
+                        "{%s}%s" % (el.get('NS'), el.get('Name'))
+                        for el in config['QNameAware'][1].findall(
+                            '{http://www.w3.org/2010/xml-c14n2}QualifiedAttr')
+                    ]
+                    qtags = [
+                        "{%s}%s" % (el.get('NS'), el.get('Name'))
+                        for el in config['QNameAware'][1].findall(
+                            '{http://www.w3.org/2010/xml-c14n2}Element')
+                    ]
+                else:
+                    qtags = qattrs = None
+
+                # Build subtest description from config.
+                config_descr = ','.join(
+                    "%s=%s" % (name, value or ','.join(c.tag.split('}')[-1] for c in children))
+                    for name, (value, children) in sorted(config.items())
+                )
+
+                with self.subTest("{}({})".format(output_file, config_descr)):
+                    if input_file == 'inNsRedecl' and not rewrite_prefixes:
+                        self.skipTest(
+                            "Redeclared namespace handling is not supported in {}".format(
+                                output_file))
+                    if input_file == 'inNsSuperfluous' and not rewrite_prefixes:
+                        self.skipTest(
+                            "Redeclared namespace handling is not supported in {}".format(
+                                output_file))
+                    if 'QNameAware' in config and config['QNameAware'][1].find(
+                            '{http://www.w3.org/2010/xml-c14n2}XPathElement') is not None:
+                        self.skipTest(
+                            "QName rewriting in XPath text is not supported in {}".format(
+                                output_file))
+
+                    out = io.StringIO()
+                    with io.open(full_path(input_file + ".xml"), 'rb') as f:
+                        if input_file == 'inC14N5':
+                            # Hack: avoid setting up external entity resolution in the parser.
+                            with open(full_path('world.txt'), 'rb') as entity_file:
+                                f = io.BytesIO(f.read().replace(b'&ent2;', entity_file.read().strip()))
+
+                        self.etree.canonicalize(
+                            out.write, file=f,
+                            comments=keep_comments,
+                            strip_text=strip_text,
+                            rewrite_prefixes=rewrite_prefixes,
+                            qname_aware_tags=qtags, qname_aware_attrs=qattrs)
+                    text = out.getvalue()
+                    with io.open(full_path(output_file + ".xml"), 'r', encoding='utf8') as f:
+                        expected = f.read()
+                    if input_file == 'inC14N3' and self.etree is not etree:
+                        # FIXME: cET resolves default attributes but ET does not!
+                        expected = expected.replace(' attr="default"', '')
+                        text = text.replace(' attr="default"', '')
+                    self.assertEqual(expected, text)
+
+
 if etree:
     class ETreeTestCase(_ETreeTestCaseBase):
         etree = etree
@@ -4645,6 +4812,9 @@ if etree:
         etree = etree
 
     class ETreeElementSlicingTest(_ElementSlicingTest):
+        etree = etree
+
+    class ETreeC14NTest(_C14NTest):
         etree = etree
 
 
@@ -4671,6 +4841,12 @@ if ElementTree:
     else:
         ElementTreePullTestCase = None
 
+    if hasattr(ElementTree, 'canonicalize'):
+        class ElementTreeC14NTest(_C14NTest):
+            etree = ElementTree
+    else:
+        ElementTreeC14NTest = None
+
     class ElementTreeElementSlicingTest(_ElementSlicingTest):
         etree = ElementTree
 
@@ -4693,10 +4869,13 @@ def test_suite():
         suite.addTests([unittest.makeSuite(ETreeTestCase)])
         suite.addTests([unittest.makeSuite(ETreePullTestCase)])
         suite.addTests([unittest.makeSuite(ETreeElementSlicingTest)])
+        suite.addTests([unittest.makeSuite(ETreeC14NTest)])
     if ElementTree:
         suite.addTests([unittest.makeSuite(ElementTreeTestCase)])
         if ElementTreePullTestCase:
             suite.addTests([unittest.makeSuite(ElementTreePullTestCase)])
+        if ElementTreeC14NTest:
+            suite.addTests([unittest.makeSuite(ElementTreeC14NTest)])
         suite.addTests([unittest.makeSuite(ElementTreeElementSlicingTest)])
     if cElementTree:
         suite.addTests([unittest.makeSuite(CElementTreeTestCase)])
