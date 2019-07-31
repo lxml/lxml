7d6de776a206fd5adf671ba1c9a85492259bc784
Cédric Krier <ced@b2ck.com>
2019-07-31 12:45:32 +0200

    Remove Python 3.4 classifier

--- setup.py
+++ setup.py
@@ -227,7 +227,6 @@ an appropriate version of Cython installed.
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
