e65a38aca21d6d502df87fc59d1fbba5e0e4a855
Cédric Krier <cedk@users.noreply.github.com>
2019-08-02 15:28:33 +0200

    Fill python_requires with proper metadata (GH-283)
    * Fill python_requires with proper metadata
    * Remove Python 3.4 classifier

--- setup.py
+++ setup.py
@@ -56,6 +56,8 @@ if versioninfo.is_pre_release():
 extra_options = {}
 if 'setuptools' in sys.modules:
     extra_options['zip_safe'] = False
+    extra_options['python_requires'] = (
+        '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, != 3.4.*')
 
     try:
         import pkg_resources
@@ -225,7 +227,6 @@ an appropriate version of Cython installed.
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
