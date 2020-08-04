ca10dbdbcc96e8b012ba67222a36df64c17577e2
Stefan Behnel <stefan_ml@behnel.de>
2020-08-04 18:19:28 +0200

    Fix an import in Py3.

--- src/lxml/html/ElementSoup.py
+++ src/lxml/html/ElementSoup.py
@@ -3,7 +3,7 @@ __doc__ = """Legacy interface to the BeautifulSoup HTML parser.
 
 __all__ = ["parse", "convert_tree"]
 
-from soupparser import convert_tree, parse as _parse
+from .soupparser import convert_tree, parse as _parse
 
 def parse(file, beautifulsoup=None, makeelement=None):
     root = _parse(file, beautifulsoup=beautifulsoup, makeelement=makeelement)
