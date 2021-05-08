a3741bc3d5b083e6503fc62ac45a48014c5ae6f4
DavidKorczynski <david@adalogics.com>
2021-05-08 14:37:11 +0100

    Add initial Atheris fuzzer. (GH-313)

---
+++ src/lxml/tests/fuzz_xml_parse.py
@@ -0,0 +1,23 @@
+"""
+Fuzzes the lxml.etree.XML function with the Atheris fuzzer.
+
+The goal is to catch unhandled exceptions and potential 
+memory corruption issues in auto-generated code.
+"""
+
+import atheris
+import sys
+
+from lxml import etree
+
+def test_etree_xml(data):
+    fdp = atheris.FuzzedDataProvider(data)
+    try:
+        root = etree.XML(fdp.ConsumeUnicode(sys.maxsize))
+    except etree.XMLSyntaxError:
+        pass
+    return
+
+if __name__ == "__main__":
+    atheris.Setup(sys.argv, test_etree_xml, enable_python_coverage=True)
+    atheris.Fuzz()
