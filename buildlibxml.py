75fbd5077de1852b6b43e1ddc70f86cefc42e08b
Niyas Sait <niyas.sait@linaro.org>
2021-11-02 10:48:45 +0000

    Fix arch variable referencing error for Py<3.5 (GH-331)

--- buildlibxml.py
+++ buildlibxml.py
@@ -38,14 +38,15 @@ def download_and_extract_windows_binaries(destdir):
         if release_path in filename
     ]
 
-    if sys.version_info < (3, 5):
-        arch = 'vs2008.' + arch
-    elif platform.machine() == 'ARM64':
+    if platform.machine() == 'ARM64':
         arch = "win-arm64"
     elif sys.maxsize > 2**32:
         arch = "win64"
     else:
         arch = "win32"
+
+    if sys.version_info < (3, 5):
+        arch = 'vs2008.' + arch
 
     libs = {}
     for libname in ['libxml2', 'libxslt', 'zlib', 'iconv']:
