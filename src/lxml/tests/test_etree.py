5269998814c847f0f89ba3be438d66837087a8f1
Kai T. Hillmann <khillman@tzi.de>
2021-11-19 17:41:35 +0100

    As suggested only tests for Python 2to3 Regression in c14n2 serialization so that the previous commit can be reverted and another approach can be tested.

--- src/lxml/tests/test_etree.py
+++ src/lxml/tests/test_etree.py
@@ -5068,6 +5068,45 @@ class ETreeC14NTestCase(HelperTestCase):
         s = etree.tostring(tree, method='c14n', exclusive=True, inclusive_ns_prefixes=['x', 'y', 'z'])
         self.assertEqual(_bytes('<a xmlns:x="http://abc" xmlns:y="http://bcd" xmlns:z="http://cde"><z:b></z:b></a>'),
                           s)
+    
+    def test_python3_problem_bytesio_iterparse(self):
+        content = BytesIO('''<?xml version="1.0" encoding="utf-8"?> <some_ns_id:some_head_elem xmlns:some_ns_id="http://www.example.com" xmlns:xhtml="http://www.w3.org/1999/xhtml"><xhtml:div></xhtml:div></some_ns_id:some_head_elem>'''.encode('utf-8'))
+        def handle_div_end(event, element):
+            if event == 'end' and element.tag.lower().startswith("{http://www.w3.org/1999/xhtml}div"):
+                # for ns_id, ns_uri in element.nsmap.items():
+                #     print(type(ns_id), type(ns_uri), ns_id, '=', ns_uri)
+                etree.tostring(element, method="c14n2")
+        for event, element in etree.iterparse(
+            source=content,
+            events=('start', 'end')
+        ):
+            handle_div_end(event, element)
+    
+    def test_python3_problem_filebased_iterparse(self):
+        with open('test.xml', 'w+b') as f:
+            f.write('''<?xml version="1.0" encoding="utf-8"?> <some_ns_id:some_head_elem xmlns:some_ns_id="http://www.example.com" xmlns:xhtml="http://www.w3.org/1999/xhtml"><xhtml:div></xhtml:div></some_ns_id:some_head_elem>'''.encode('utf-8'))
+        def handle_div_end(event, element):
+            if event == 'end' and element.tag.lower() == "{http://www.w3.org/1999/xhtml}div":
+                # for ns_id, ns_uri in element.nsmap.items():
+                #     print(type(ns_id), type(ns_uri), ns_id, '=', ns_uri)
+                etree.tostring(element, method="c14n2")
+        for event, element in etree.iterparse(
+            source='test.xml',
+            events=('start', 'end')
+        ):
+            handle_div_end(event, element)
+    
+    def test_python3_problem_filebased_parse(self):
+        with open('test.xml', 'w+b') as f:
+            f.write('''<?xml version="1.0" encoding="utf-8"?> <some_ns_id:some_head_elem xmlns:some_ns_id="http://www.example.com" xmlns:xhtml="http://www.w3.org/1999/xhtml"><xhtml:div></xhtml:div></some_ns_id:some_head_elem>'''.encode('utf-8'))
+        def serialize_div_element(element):        
+            # for ns_id, ns_uri in element.nsmap.items():
+            #     print(type(ns_id), type(ns_uri), ns_id, '=', ns_uri)
+            etree.tostring(element, method="c14n2")
+        tree = etree.parse(source='test.xml')
+        root = tree.getroot()
+        div = root.xpath('//xhtml:div', namespaces={'xhtml':'http://www.w3.org/1999/xhtml'})[0]
+        serialize_div_element(div)
 
 
 class ETreeWriteTestCase(HelperTestCase):
