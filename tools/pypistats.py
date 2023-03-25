e5a330c01cfcfb7bfc13fb0dd644a5bb761e1bfb
Stefan Behnel <stefan_ml@behnel.de>
2023-03-25 21:24:46 +0100

    Print download totals in pypistats.

--- tools/pypistats.py
+++ tools/pypistats.py
@@ -43,8 +43,7 @@ def system_sorter(name_and_count):
         return len(order)
 
 
-def print_agg_stats(counts, sort_key=None):
-    stats = aggregate(counts)
+def print_agg_stats(stats, sort_key=None):
     total = sum(stats.values())
     max_len = max(len(category) for category in stats)
     agg_sum = 0.0
@@ -58,13 +57,19 @@ def main():
     package_name = sys.argv[1] if len(sys.argv) > 1 else PACKAGE
 
     counts = get_stats("python_minor", package=package_name)
+    stats = aggregate(counts)
     print("Downloads by Python version:")
-    print_agg_stats(counts, sort_key=version_sorter)
+    print_agg_stats(stats, sort_key=version_sorter)
 
     print()
     counts = get_stats("system", package=package_name)
+    stats = aggregate(counts)
     print("Downloads by system:")
-    print_agg_stats(counts, sort_key=system_sorter)
+    print_agg_stats(stats, sort_key=system_sorter)
+
+    total = sum(stats.values())
+    days = {"month": 30, "week": 7, "day": 1}
+    print(f"Total downloads: {total * days['month']:-12,.1f}")
 
 
 if __name__ == '__main__':
