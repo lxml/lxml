f05a5e3c1a4dd9f829b9f41725a9ba79a3d13a80
Stefan Behnel <stefan_ml@behnel.de>
2023-03-25 11:52:48 +0100

    Build universal x86_64 + Arm64 wheels on macOS by default.

--- buildlibxml.py
+++ buildlibxml.py
@@ -420,21 +420,14 @@ def cmmi(configure_cmd, build_dir, multicore=None, **call_setup):
 
 def configure_darwin_env(env_setup):
     import platform
-    # configure target architectures on MacOS-X (x86_64 only, by default)
+    # configure target architectures on MacOS-X (x86_64 + Arm64, by default)
     major_version, minor_version = tuple(map(int, platform.mac_ver()[0].split('.')[:2]))
-    if major_version > 7:
-        if platform.mac_ver()[2] == "arm64":
-            env_default = {
-                'CFLAGS': "-arch arm64 -O2",
-                'LDFLAGS': "-arch arm64",
-                'MACOSX_DEPLOYMENT_TARGET': "11.0"
-            }
-        else:
-            env_default = {
-                'CFLAGS': "-arch x86_64 -O2",
-                'LDFLAGS': "-arch x86_64",
-                'MACOSX_DEPLOYMENT_TARGET': "10.6"
-            }
+    if major_version >= 11:
+        env_default = {
+            'CFLAGS': "-arch x86_64 -arch arm64 -O3",
+            'LDFLAGS': "-arch x86_64 -arch arm64",
+            'MACOSX_DEPLOYMENT_TARGET': "11.0"
+        }
         env_default.update(os.environ)
         env_setup['env'] = env_default
 
