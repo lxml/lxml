61d326b0f3ccc28eb32e11741536fe4b86d9fdb9
Stefan Behnel <stefan_ml@behnel.de>
2023-03-29 14:10:55 +0200

    Update supported versions in package classifiers.

--- setup.py
+++ setup.py
@@ -257,7 +257,6 @@ an appropriate version of Cython installed.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
