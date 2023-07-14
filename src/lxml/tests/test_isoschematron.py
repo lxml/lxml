57d328d611ea5b3f49a6c132617e37f29a36525e
Miro Hrončok <miro@hroncok.cz>
2023-07-14 19:45:48 +0200

    Skip "test_isoschematron.test_schematron_invalid_schema_empty" without the RNG file (GH-380)
    The expected SchematronParseError only happens when validate_schema is true.

--- src/lxml/tests/test_isoschematron.py
+++ src/lxml/tests/test_isoschematron.py
@@ -55,6 +55,8 @@ class ETreeISOSchematronTestCase(HelperTestCase):
         schema = isoschematron.Schematron(schema)
         self.assertTrue(schema)
 
+    @unittest.skipIf(not isoschematron.schematron_schema_valid_supported,
+                     'SchematronParseError is risen only when validate_schema is true')
     def test_schematron_invalid_schema_empty(self):
         schema = self.parse('''\
 <schema xmlns="http://purl.oclc.org/dsdl/schematron" />
