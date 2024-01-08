aeb0da23da1c65a313011a58291c1e25260e38bd
Stefan Behnel <stefan_ml@behnel.de>
2024-01-08 10:38:57 +0100

    Add temporary fix for building with old MSCV versions (specifically, in Py2.7).

--- setupinfo.py
+++ setupinfo.py
@@ -180,6 +180,34 @@ def ext_modules(static_include_dirs, static_library_dirs,
         from Cython.Build import cythonize
         result = cythonize(result, compiler_directives=cythonize_directives)
 
+        # Temporary fix for Cython 3.0.7
+        if Cython.__version__.startswith('3.0.'):
+            for ext in result:
+                source_file = ext.sources[0]
+                fixed = False
+                lines = []
+                with open(source_file, encoding='iso8859-1') as f:
+                    source_lines = iter(f)
+                    for line in source_lines:
+                        lines.append(line)
+                        if 'SelflessCall' in line:
+                            break
+                    for line in source_lines:
+                        if line == "    PyObject *selfless_args = PyTuple_GetSlice(args, 1, PyTuple_Size(args));\n":
+                            line = "    PyObject *result, *selfless_args = PyTuple_GetSlice(args, 1, PyTuple_Size(args));\n"
+                        elif line.startswith("    PyObject *result = "):
+                            line = "    " + line[len("    PyObject *"):]
+                            fixed = True
+                        lines.append(line)
+                        if "return result" in line:
+                            break
+                    if fixed:
+                        lines.extend(source_lines)
+                if fixed:
+                    print("Applying work-around for Cython 3.0.x to %s" % source_file)
+                    with open(source_file, mode='w', encoding='iso8859-1') as f:
+                        f.writelines(lines)
+
     # for backwards compatibility reasons, provide "etree[_api].h" also as "lxml.etree[_api].h"
     for header_filename in HEADER_FILES:
         src_file = os.path.join(SOURCE_PATH, 'lxml', header_filename)
