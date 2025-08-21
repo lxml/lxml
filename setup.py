457c56428d63e470fe0e679f2232545d6da8e7da
Stefan Behnel <stefan_ml@behnel.de>
2025-08-21 15:16:05 +0200

    Build: Mark Py3.14 as officially supported.

--- setup.py
+++ setup.py
@@ -240,6 +240,7 @@ build the sources, see the build instructions on the project home page.
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Programming Language :: C',
         'Operating System :: OS Independent',
         'Topic :: Text Processing :: Markup :: HTML',
